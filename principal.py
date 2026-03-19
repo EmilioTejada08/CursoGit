b03dac9f71fa5c6fe4bf80fa0d43d5ab7c45db0e
EmilioTejada08 <tejadaemi08@gmail.com>
2026-03-19 22:59:55 +0100

    Todo agregado

---
+++ principal.py
@@ -0,0 +1,6 @@
+from funciones.matematicas import *
+print ("Bienvenido a mi programa.")
+num1=int(input("Introduce un número: "))
+num2=int(input("Introduce otro número: "))
+print("------------------------------")
+suma(num1,num2)
