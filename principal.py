7488322ec42392a7129919fdba39f18857524ae8
EmilioTejada08 <tejadaemi08@gmail.com>
2026-03-24 19:24:02 +0100

    Añadido el módulo y la división entera

--- principal.py
+++ principal.py
@@ -25,5 +25,9 @@ match op:
     case "raíz":
         print(f"La raíz cuadrada de {num1} es {math.sqrt(num1)}")
         print(f"La raíz cuadrada de {num2} es {math.sqrt(num2)}")
+    case "módulo":
+        funciones.matematicas.modulo(num1, num2)
+    case "División entera":
+        funciones.matematicas.division_entera(num1, num2)
     case _:
         print("Operación no reconocida.")
